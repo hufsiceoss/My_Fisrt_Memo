a1f67470dbfc0862752eecee3f0275a852d483df
jinyoung moon <mjy9650@naver.com>
2020-07-01 04:05:48 +0900

    최종버전으로 깃허브에 올리기 위해서 각 클래스에 약간의 각주 추가 후 푸쉬

--- app/src/main/java/com/professionalandroid/apps/my_fisrt_memo/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/my_fisrt_memo/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.professionalandroid.apps.my_fisrt_memo.R;
+import com.professionalandroid.apps.my_fisrt_memo.TextFileManager;
 
 public class MainActivity extends AppCompatActivity {
     EditText mMemoEdit = null;
@@ -43,5 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "삭제삭제", Toast.LENGTH_LONG).show();
             }
         }
-    }
+    }//클릭할때 어떻게 실행되는지에 관한 함수 불러오기, 저장하기, 삭제하기 등을 버튼으로 만들었고
+     //각 버튼을 눌렀을 때 TextFileManager 클래스와 연동될수 있게 불러오며 일정 행동을 수행한 후에
+     //밑에 작은 문구로 실행을 완료했음을 토스트로 화면에 보여준다.
 }
--- app/src/main/java/com/professionalandroid/apps/my_fisrt_memo/TextFileManager.java
+++ app/src/main/java/com/professionalandroid/apps/my_fisrt_memo/TextFileManager.java
@@ -28,7 +28,8 @@ public class TextFileManager {
         } catch(Exception e) {
             e.printStackTrace();
         }
-    }
+    }//저장하기 함수 작성했던 메모를 저장한다.
+     //메모를 저장할 때는 바이트 타입으로 저장한다.
 
     public String load() {
         try {
@@ -42,9 +43,10 @@ public class TextFileManager {
         } catch (IOException e) {
         }
         return "";
-    }
+    }//불러오기 함수 사용자가 저장했던 메모를 불러 다시 화면에 보여준다.
+     //바이트를 다시 스트링타입으로 사용자에게 변환하여 보여준다.
 
     public void delete() {
         mContext.deleteFile(FILE_NAME);
-    }
+    }//삭제하기 함수 저장했던 함수를 삭제해준다.
 }
